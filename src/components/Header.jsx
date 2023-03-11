6eae624dab9c33860f4d64382aa8186298d85893
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-11 12:46:38 +0200

    добавил хедер

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Logo = styled.p`
+  font-family: "Golos Text", sans-serif;
+  font-weight: 700;
+  margin-bottom: 0;
+  font-size: 45px;
+`;
 
 const Header = () => {
   return (
     <>
-     <nav className="navbar navbar-dark bg-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          <img
-            src="/docs/5.2/assets/brand/bootstrap-logo.svg"
-            alt="Logo"
-           
-            className="d-inline-block align-text-top"
-          />
-         
-        </a>
-      </div>
-    </nav>
-    <Outlet/>
+      <nav className="navbar navbar-dark bg-dark">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            <Logo>Q</Logo>
+          </Link>
+        </div>
+      </nav>
+      <Outlet />
     </>
-   
   );
 };
 export default Header;
