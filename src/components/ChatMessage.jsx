e51aaddb7309936344910f61ffcd1ed6f2feaa96
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-14 19:12:09 +0200

    добавляю аватарки из стреджа

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,23 +1,38 @@
-import React, { useEffect, useRef } from 'react';
-import { auth } from '../firebase';
+import React, { useEffect, useRef, useState } from "react";
+import { auth, storage } from "../firebase";
+import { ref, getDownloadURL } from "firebase/storage";
 
 const ChatMessage = (props) => {
   const messagesEndRef = useRef(null);
+  const { text, uid } = props.message;
+  const [avatarURL, setAvatarUrl] = useState("");
 
+  
+
+  const scrollToBottom = () => {
+    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    // Get the avatar download URL for the current user
+    const storageRef = ref(storage, `avatars/${uid}`);
+    getDownloadURL(storageRef)
+      .then((url) => {
+        setAvatarUrl(url);
+      })
+      .catch((error) => {
+        console.error("Error getting avatar download URL:", error);
+      });
+  }, [uid]);
   useEffect(() => {
     scrollToBottom();
   }, []);
 
-   const scrollToBottom = () => {
-    messagesEndRef.current.scrollIntoView({ behavior: "smooth" });
-  }
-
-  const { text, uid, photoURL } = props.message;
 
   const messageClass = uid === auth.currentUser.uid ? "sent" : "received";
   return (
     <div ref={messagesEndRef} className={`message ${messageClass}`}>
-        <img src={photoURL}  />
+      <img src={avatarURL} />
       <p className="chat_msg">{text}</p>
     </div>
   );
