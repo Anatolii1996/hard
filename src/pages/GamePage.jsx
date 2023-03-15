bfb3092c0db16f4b2d72b24af16c71113344c7fc
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-15 19:32:55 +0200

    сверстал макет игрового поля

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,16 +1,26 @@
 import { GoCheck } from "react-icons/go";
 import { RxCross2 } from "react-icons/rx";
 
-const GamePage=()=>{
-return(
+const GamePage = () => {
+  return (
     <div className="game_page_wrap">
-        <div className="game_buttons">
-        <GoCheck/>
-        <RxCross2/>
+      <div className="game_buttons">
+        <GoCheck />
+        <RxCross2 />
+      </div>
+      <div className="game_area">
+        <p className="qustion_text">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae, nam.
+        </p>
+        <div className="answer_buttons_wrap">
+            <button className="answer_may">lorem</button>
+        <button className="answer_may">lorem</button>
+        <button className="answer_may">lorem</button>
+        <button className="answer_may">lorem</button>
         </div>
-        <div className="game_area"> <h1>TEST</h1></div>
-       
+        
+      </div>
     </div>
-)
+  );
 };
-export default GamePage
+export default GamePage;
