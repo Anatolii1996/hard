c0cacb6da5d1c73800f73992efb36b8142b08327
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-15 19:20:09 +0200

    добавил игровые кнопки

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,9 +1,15 @@
-
+import { GoCheck } from "react-icons/go";
+import { RxCross2 } from "react-icons/rx";
 
 const GamePage=()=>{
 return(
     <div className="game_page_wrap">
-        <h1>TEST</h1>
+        <div className="game_buttons">
+        <GoCheck/>
+        <RxCross2/>
+        </div>
+        <div className="game_area"> <h1>TEST</h1></div>
+       
     </div>
 )
 };
