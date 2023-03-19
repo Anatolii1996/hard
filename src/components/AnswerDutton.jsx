48915dcb1680ccd44f767d133e15e1457af13c03
Anatolii1996 <anatoly.tka4enko2014@gmail.com>
2023-03-19 09:46:52 +0200

    поменял цвет текста кнопки ответа

--- src/components/AnswerDutton.jsx
+++ src/components/AnswerDutton.jsx
@@ -1,6 +1,7 @@
 const AnswerButon = ({ el, selected, setSelected }) => {
   return (
     <button
+    
     className={selected ? `answer_may selected` : `answer_may `}
     onClick={() =>setSelected(el)}
     >
